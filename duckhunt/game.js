fdd161a780e50572588f8e066fea17ab01819c5d
Rohan <ron4trj@gmail.com>
2014-02-17 22:08:10 -0500

    added drawImage but does not work

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,7 +1,10 @@
 // Your work goes here...
+var sprites= new Image();
+sprites.src="./assets/duckhunt.png";
 
-function draw() {
+function Draw() {
+	var canvas = document.getElementById('game');
+	var ctx = canvas.getContext('2d');
+	ctx.drawImage(sprites, 0, 710, 800, 190, 0, 410);
 
-var canvas = document.getElementById('game');
-var ctx = canvas.getContext('2d');
-var img =getElementById("picture");
+}
