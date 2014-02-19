0ed77f31b7522604034641fde88d605726dd9e69
Rohan <ron4trj@gmail.com>
2014-02-19 18:49:13 -0500

    added dirt and grass to canvas

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -2,9 +2,9 @@
 var sprites= new Image();
 sprites.src="./assets/duckhunt.png";
 
-function Draw() {
+ function Draw() {
 	var canvas = document.getElementById('game');
 	var ctx = canvas.getContext('2d');
-	ctx.drawImage(sprites, 0, 710, 800, 190, 0, 410);
+	ctx.drawImage(sprites, 0, 710, 800, 190, 0, 500, 800, 100);
 
 }
