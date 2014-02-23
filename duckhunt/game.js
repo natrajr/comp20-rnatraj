315dc4ad229124ee5201d92a07495142b3cb2f7c
Rohan <ron4trj@gmail.com>
2014-02-23 18:31:24 -0500

    added tree

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -5,6 +5,9 @@ sprites.src="./assets/duckhunt.png";
  function Draw() {
 	var canvas = document.getElementById('game');
 	var ctx = canvas.getContext('2d');
+	//drawImage(img, sx, sy, sW, sH, dx, dy, dW, dH)
+	//tree slice
+	ctx.drawImage(sprites, 4, 272, 78, 124, 100, 445, 80, 100);
+	//ground slice
 	ctx.drawImage(sprites, 0, 710, 800, 190, 0, 500, 800, 100);
-
 }
