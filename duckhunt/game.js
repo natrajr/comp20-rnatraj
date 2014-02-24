82b6db3386ef517514750e969bc9e77589d8cf41
Rohan <ron4trj@gmail.com>
2014-02-23 19:34:43 -0500

    added birds

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -12,4 +12,6 @@ sprites.src="./assets/duckhunt.png";
 	ctx.drawImage(sprites, 0, 710, 800, 190, 0, 440, 800, 160);
 	//dog slice
 	ctx.drawImage(sprites, 0, 0, 60, 50, 150, 510, 70, 80);
+	//the birds
+	ctx.drawImage(sprites, 40, 120, 208, 26, 30, 100, 350, 30);
 }
