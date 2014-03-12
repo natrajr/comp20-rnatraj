5c3c24421ce6f2f039fb8adc4bc4eb507925eb2d
Rohan Natraj <ron4trj@gmail.com>
2014-03-12 00:34:18 -0400

    added navigation.geolocation and map rendering

--- transit/maps.js
+++ transit/maps.js
@@ -1,11 +1,58 @@
 //maps.js
 
-	
-Function init() {
-	var mapOptions = {
-  		center: new google.maps.LatLng(-34.397, 150.644),
-  		zoom: 8
+
+//my position
+var myLoc= new google.maps.LatLng(myLat, myLng);
+var myLng=0;
+var myLat=0;
+
+
+var mapOptions = {
+  		center: myLoc,
+  		zoom: 10
+  		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 
-	var map =new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+var theMap;
+var marker;
+var infoWindow=new google.maps.InfoWindow();
+
+function init_map() {
+	theMap =new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	getMyLocation();
+}
+
+function getMyLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			myLat=position.coords.latitude;
+			myLng=position.coords.longitude;
+			renderMap();
+		}
+	}
+	else {
+		alert("Your Browser Does Not Support Geolocation");
+	}
+}
+
+
+function renderMap() {
+
+	myLoc=new google.maps.LatLng(myLat,myLng);
+	theMap.panTo(myLoc);
+	
+	marker=new google.maps.Marker({
+		position: myLoc,
+		title: "You are Here"
+	});
+
+	marker.setMap(myLoc);
+
+	google.maps.event.addListener(marker, 'click', function() {
+					infowindow.setContent(marker.title);
+					infowindow.open(theMap, marker);
+				});
+
+
 }
