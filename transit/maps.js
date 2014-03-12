63af3c39e3ec702bd5208fa9c1fb25838753fd3e
Rohan Natraj <ron4trj@gmail.com>
2014-03-11 22:22:11 -0400

    added map object

--- transit/maps.js
+++ transit/maps.js
@@ -1,2 +1,11 @@
 //maps.js
 
+	
+Function init() {
+	var mapOptions = {
+  		center: new google.maps.LatLng(-34.397, 150.644),
+  		zoom: 8
+	};
+
+	var map =new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+}
